f95b86dd94dbad04575f8946f63f8098390f12c2
LucasValpereiro <lucasvalpereiro@hotmail.com>
2026-01-05 12:40:51 -0300

    added header and links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,13 @@
+import './header.css';
+import { Link } from 'react-router-dom';
+
 function Header(){
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
-    )
-}
+        <header>
+            <Link className='logo' to="/">Prime Flix</Link>
+            <Link className='favoritos' to="/favoritos">Meus Filmes</Link>
+        </header>
+    );
+};
 
 export default Header;
