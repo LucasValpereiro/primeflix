be22d859bad8d32cea60caebc921076e6e044c17
LucasValpereiro <lucasvalpereiro@hotmail.com>
2026-01-05 15:40:13 -0300

    added imports and infos about the movies

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 // URL DA API: https://api.themoviedb.org/3/movie/now_playing?api_key=228812bb251e0fa1dcd821e1c6ccb2db&language=pt-BR
 
 function Home(){
-    const {filmes, setFilmes} = useState([]);
+    const [filmes, setFilmes] = useState([]);
 
 
     useEffect(() => {
@@ -18,7 +20,9 @@ function Home(){
                 }
             })
 
-            console.log(response)
+         //   console.log(response.data.results.slice(0, 10));
+
+         setFilmes(response.data.results.slice(0, 10));
 
         }
 
@@ -27,8 +31,18 @@ function Home(){
     }, [])
 
     return(
-        <div>
-        <h1>Bem vindo a Home!</h1>
+        <div className="container">
+            <div className="lista-filmes">
+            {filmes.map((filme) => {
+                return(
+                    <article key={filme.id}>
+                        <strong>{filme.title}</strong>
+                        <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                        <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                    </article>
+                )
+            })}
+            </div>
         </div>
     );
 };
