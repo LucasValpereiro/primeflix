7e193cb75884438bb0306e4a2c9a67f7c798abe9
LucasValpereiro <lucasvalpereiro@hotmail.com>
2026-01-06 20:33:08 -0300

    added styles and trailer search

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,70 @@
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate, useNavigation } from 'react-router-dom';
+import './filme-info.css';
+import api from '../../services/api';
+
 function Filme (){
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+ 
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key: '228812bb251e0fa1dcd821e1c6ccb2db',
+                    language: 'pt-BR',
+                }
+            })
+            .then((response) => {
+                setFilme(response.data)
+                setLoading(false);
+            })
+            .catch(() => {
+                console.log('Filme não encontrado!')
+                navigate('/', { replace: true })
+                return;
+            })
+        }
+
+        loadFilme();
+
+
+        return() => {
+            console.log('Componente foi desmontado')
+        }
+    }, [navigate, id]);
+
+    if(loading) {
+        return(
+            <div className='filme-info'>
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
     return(
-        <div>
-        <h1>Bem vindo aos filmes!</h1>
+        <div className='filme-info'>
+        <h1>{filme.title}</h1>
+        <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+        <h3>Sinopse</h3>
+        <span>{filme.overview}</span>
+
+        <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
+
+
+        <div className='area-buttons'>
+        <button>Salvar</button>
+        <button>
+            <a target='_blank' rel='external' href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
+                Trailer
+            </a>
+        </button>
+        </div>
+
         </div>
     );
 };
